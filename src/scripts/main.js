fe8ac2f992deb706f25803137574cf043de3af0d
joãoA <joaovictor.dudu03@gmail.com>
2025-07-27 22:10:46 -0300

    js adicionado e validado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
-console.log('ola mundo')
-console.log('ola mundo2')
-console.log('ola mundo3')
-console.log('ola mundo4')
-console.log('ola mundo5')
-console.log('ola mundo6')
-console.log('ola mundo7')
+const result = document.getElementById('result')
+const count = document.getElementById('count')
+const start = document.getElementById('start')
+
+
+start.addEventListener('click', function(){
+
+    getAdvice()
+
+
+
+})
+
+
+async function getAdvice() {
+    const response = await fetch('https://api.adviceslip.com/advice')
+    const data = await response.json()
+    result.innerHTML = `"${data.slip.advice}"`
+    count.innerHTML = data.slip.id
+    console.log(data)
+}
+
