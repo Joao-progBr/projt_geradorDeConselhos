56ec558d47b2815bfe96b267558d51a38b39104b
joãoA <joaovictor.dudu03@gmail.com>
2025-07-28 21:00:41 -0300

    adicionando a função typeText para uma animção e estilizando no js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,33 @@ const start = document.getElementById('start')
 
 
 start.addEventListener('click', function(){
-
     getAdvice()
-
-
-
 })
 
 
 async function getAdvice() {
     const response = await fetch('https://api.adviceslip.com/advice')
     const data = await response.json()
-    result.innerHTML = `"${data.slip.advice}"`
+    const advice = `"${data.slip.advice}"`
     count.innerHTML = data.slip.id
+    typeText(result, advice, 40)
     console.log(data)
+    // result.innerHTML = `"${data.slip.advice}"`
 }
+
+
+function typeText(element, text, speed = 30){
+    element.innerHTML = ''
+    let index = 0
+
+    const interval = setInterval(() => {
+        element.innerHTML += text.charAt(index)
+        index++
+
+        if(index >= text.length){
+            clearInterval(interval)
+        }
+    }, speed)
+}
+
 
